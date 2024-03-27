e34dcb00aa3120a8dabb3ab27671dfe4e52e5a3c
foehammer127 <foehammer@disroot.org>
2024-03-26 19:35:10 -0500

    init: setup pocketbase

---
+++ cmd/newsletter/main.go
@@ -0,0 +1,26 @@
+package main
+
+import (
+	"log"
+	"os"
+	"strings"
+
+	"github.com/pocketbase/pocketbase"
+	"github.com/pocketbase/pocketbase/plugins/migratecmd"
+)
+
+func main() {
+	app := pocketbase.New()
+
+	isGoRun := strings.HasPrefix(os.Args[0], os.TempDir())
+
+	migratecmd.MustRegister(app, app.RootCmd, migratecmd.Config{
+		// enable auto creation of migration files when making collection changes in the Admin UI
+		// (the isGoRun check is to enable it only during development)
+		Automigrate: isGoRun,
+	})
+
+	if err := app.Start(); err != nil {
+		log.Fatal(err)
+	}
+}
