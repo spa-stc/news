9358f3c52969a20da3ed90b1d2f6db492022f47d
foehammer127 <foehammer@disroot.org>
2024-03-26 19:45:48 -0500

    feat(collections/users): remove avatar collection

---
+++ migrations/1711500326_updated_users.go
@@ -0,0 +1,63 @@
+package migrations
+
+import (
+	"encoding/json"
+
+	"github.com/pocketbase/dbx"
+	"github.com/pocketbase/pocketbase/daos"
+	m "github.com/pocketbase/pocketbase/migrations"
+	"github.com/pocketbase/pocketbase/models/schema"
+)
+
+func init() {
+	m.Register(func(db dbx.Builder) error {
+		dao := daos.New(db);
+
+		collection, err := dao.FindCollectionByNameOrId("_pb_users_auth_")
+		if err != nil {
+			return err
+		}
+
+		// remove
+		collection.Schema.RemoveField("users_avatar")
+
+		return dao.SaveCollection(collection)
+	}, func(db dbx.Builder) error {
+		dao := daos.New(db);
+
+		collection, err := dao.FindCollectionByNameOrId("_pb_users_auth_")
+		if err != nil {
+			return err
+		}
+
+		// add
+		del_avatar := &schema.SchemaField{}
+		if err := json.Unmarshal([]byte(`{
+			"system": false,
+			"id": "users_avatar",
+			"name": "avatar",
+			"type": "file",
+			"required": false,
+			"presentable": false,
+			"unique": false,
+			"options": {
+				"mimeTypes": [
+					"image/jpeg",
+					"image/png",
+					"image/svg+xml",
+					"image/gif",
+					"image/webp"
+				],
+				"thumbs": null,
+				"maxSelect": 1,
+				"maxSize": 5242880,
+				"protected": false
+			}
+		}`), del_avatar); err != nil {
+			return err
+		}
+		collection.Schema.AddField(del_avatar)
+
+		return dao.SaveCollection(collection)
+	})
+}
