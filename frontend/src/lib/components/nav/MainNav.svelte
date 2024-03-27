2c8d0f8bb33bee72a3de08841dcb5a05e43fdc68
foehammer127 <foehammer@disroot.org>
2024-03-27 15:54:01 -0500

    feat(frontend): change favicon and title

--- frontend/src/lib/components/nav/MainNav.svelte
+++ frontend/src/lib/components/nav/MainNav.svelte
@@ -1,9 +1,10 @@
 <script>
   import { Newspaper } from "lucide-svelte";
+  import { link } from "svelte-spa-router";
 </script>
 
 <div class="flex gap-6 md:gap-10">
-  <a href="/" class="flex items-center space-x-2">
+  <a use:link href="/" class="flex items-center space-x-2">
     <Newspaper />
     <span class="font-bold hidden sm:inline-block">SPA Student Newsletter</span>
   </a>
