7ad8e13cf3da65b125673e53ebb57963d2940410
foehammer127 <foehammer@disroot.org>
2024-03-29 16:27:07 -0500

    feat(frontend): add dropdown menu to auth picture in the frontend

--- frontend/src/lib/components/nav/ProfileMenu.svelte
+++ frontend/src/lib/components/nav/ProfileMenu.svelte
@@ -1,15 +1,30 @@
 <script lang="ts">
   import * as Avatar from "$lib/components/ui/avatar";
+  import * as DropdownMenu from "$lib/components/ui/dropdown-menu";
+  import { pb } from "$lib/pocketbase";
   import toast from "svelte-french-toast";
-  import Button from "../ui/button/button.svelte";
 
   export let username: string;
 </script>
 
-<!-- <Button variant="outline" size="icon"> -->
-<Avatar.Root>
-  <Avatar.Image src="https://api.dicebear.com/8.x/thumbs/svg?seed={username}"
-  ></Avatar.Image>
-  <Avatar.Fallback>{username}</Avatar.Fallback>
-</Avatar.Root>
-<!-- </Button> -->
+<DropdownMenu.Root>
+  <DropdownMenu.Trigger>
+    <Avatar.Root>
+      <Avatar.Image
+        src="https://api.dicebear.com/8.x/thumbs/svg?seed={username}"
+      ></Avatar.Image>
+      <Avatar.Fallback>{username}</Avatar.Fallback>
+    </Avatar.Root>
+  </DropdownMenu.Trigger>
+  <DropdownMenu.Content>
+    <DropdownMenu.Group>
+      <DropdownMenu.Label>Account</DropdownMenu.Label>
+      <DropdownMenu.Item
+        on:click={() => {
+          pb.authStore.clear();
+          toast.success("Logged Out!");
+        }}>Log Out</DropdownMenu.Item
+      >
+    </DropdownMenu.Group>
+  </DropdownMenu.Content>
+</DropdownMenu.Root>
