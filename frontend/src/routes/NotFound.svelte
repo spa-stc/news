3e31d828ff72356ba5f0a1335cb890260e6c5e2a
foehammer127 <foehammer@disroot.org>
2024-03-30 21:48:59 -0500

    feat(frontend): add better 404 page

---
+++ frontend/src/routes/NotFound.svelte
@@ -0,0 +1,14 @@
+<script>
+  import Button from "$lib/components/ui/button/button.svelte";
+</script>
+
+<div
+  class="container-sm mx-auto mt-12 flex flex-col px-4 justify-between space-y-4"
+>
+  <h1 class="font-bold text-xl">Page Not Found:</h1>
+  <img
+    src="https://i.pinimg.com/originals/54/52/47/545247bb438ef0d423b92c8683e67abd.gif"
+    alt="Kermit Slap"
+  />
+  <Button href="#/">Go Home</Button>
+</div>
