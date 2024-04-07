26724678a1c1e1f54cbcd9168785a4be5e0370ff
foehammer127 <foehammer@disroot.org>
2024-04-07 17:19:51 -0500

    feat(nixos): add github runner ssh key

--- nixos/common/default.nix
+++ nixos/common/default.nix
@@ -23,8 +23,11 @@
 
     users.users.root = {
       openssh.authorizedKeys.keys = [
-        # Foehammer.
+        # Foehammer
         "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIAP0Hp1XEQ0Fx4rpuBuH7YusGU1f6s3Q4Sb1O1U0oKWA foehammer@disroot.org"
+
+        # Github Actions Runner
+        "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIBwc6ZjZBYzW2bxiwIvJ1W99RQU08rcap7Fj1q8c3pVc runner@github.io"
       ];
     };
   };
