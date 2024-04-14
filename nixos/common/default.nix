ef653f0916171893cb3cf0b413fb3834d37a81df
foehammer127 <foehammer@disroot.org>
2024-04-14 16:32:44 -0500

    feat(nixos): add foehammer yubikey to root authorized keys

--- nixos/common/default.nix
+++ nixos/common/default.nix
@@ -27,6 +27,7 @@
       openssh.authorizedKeys.keys = [
         # Foehammer
         "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIAP0Hp1XEQ0Fx4rpuBuH7YusGU1f6s3Q4Sb1O1U0oKWA foehammer@disroot.org"
+        "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIKtCcKS1jN/WSb4Ggvpz11pkUdE4kMgrN0xwptXLgSBX openpgp:0x79EFAC41"
 
         # Github Actions Runner
         "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIBwc6ZjZBYzW2bxiwIvJ1W99RQU08rcap7Fj1q8c3pVc runner@github.io"
