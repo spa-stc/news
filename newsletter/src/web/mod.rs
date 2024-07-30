ea87f5896af6481f21b9af61e626bbad0084321c
foehammer127 <foehammer@disroot.org>
2024-07-30 00:26:15 -0500

    Add Request Logging
    Add logging of requests, since it is best practice.

--- newsletter/src/web/mod.rs
+++ newsletter/src/web/mod.rs
@@ -1,4 +1,10 @@
-use axum::routing::get;
+use axum::{
+    extract::Request,
+    http::{header, StatusCode},
+    middleware::{self, Next},
+    response::Response,
+    routing::get,
+};
 use resources::Resources;
 use statics::{serve_root_statics, serve_statics};
 use tokio::net::TcpListener;
@@ -18,6 +24,7 @@ pub async fn start_server(listener: Listener, resources: Resources) -> Result<()
         .route("/healthz", get(healthz))
         .route("/static/:file", get(serve_statics))
         .route("/:file", get(serve_root_statics))
+        .route_layer(middleware::from_fn(request_trace))
         .with_state(resources.clone());
 
     match listener {
@@ -31,4 +38,30 @@ pub async fn start_server(listener: Listener, resources: Resources) -> Result<()
 
 async fn healthz() -> &'static str {
     "Service Ready."
+}
+
+async fn request_trace(req: Request, next: Next) -> Result<Response, StatusCode> {
+    let uri = req.uri().to_string();
+
+    let ua = req
+        .headers()
+        .get(header::USER_AGENT)
+        .map(|s| String::from_utf8_lossy(s.as_bytes()));
+
+    let ip = req
+        .headers()
+        .get("x-forwarded-for")
+        .map(|s| String::from_utf8_lossy(s.as_bytes()));
+
+    let r = req
+        .headers()
+        .get(header::REFERER)
+        .map(|s| String::from_utf8_lossy(s.as_bytes()));
+
+    tracing::info!(
+        "http_request {}",
+        serde_json::json!({ "uri": uri, "ua": ua, "ip": ip, "r": r })
+    );
+
+    Ok(next.run(req).await)
 }
