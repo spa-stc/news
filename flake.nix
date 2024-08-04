60d4d634243f2f86afa30ec12813993ee13f8066
foehammer127 <foehammer@disroot.org>
2024-08-04 17:40:52 -0500

    Switch To Golang
    Switch back to golang, since building apps in rust sucks.

--- flake.nix
+++ flake.nix
@@ -8,15 +8,6 @@
       url = "github:kamadorueda/alejandra/3.0.0";
       inputs.nixpkgs.follows = "nixpkgs";
     };
-
-    rust-overlay = {
-      url = "github:oxalica/rust-overlay";
-      inputs.nixpkgs.follows = "nixpkgs";
-    };
-    naersk = {
-      url = "github:nix-community/naersk";
-      inputs.nixpkgs.follows = "nixpkgs";
-    };
   };
 
   outputs = inputs @ {self, ...}:
@@ -30,68 +21,29 @@
         pkgs,
         system,
         ...
-      }: let
-        rustToolchain = pkgs.rust-bin.fromRustupToolchainFile ./rust-toolchain.toml;
-
-        naersk = inputs.naersk.lib.${system}.override {
-          cargo = rustToolchain;
-          rustc = rustToolchain;
-        };
-
-        buildInputs = with pkgs; [
-          sqlite.dev
-          openssl
-        ];
-
-        naitiveBuildInputs = with pkgs; [
-          pkg-config
-        ];
-      in {
+      }: {
         _module.args.pkgs = import inputs.nixpkgs {
           localSystem = system;
           config = {
             allowUnfree = true;
             allowAliases = true;
           };
-          overlays = [
-            inputs.rust-overlay.overlays.default
-          ];
+          overlays = [];
         };
 
         devShells.default = pkgs.mkShell {
-          buildInputs = with pkgs;
-            [
-              just
-              rustToolchain
-              sqlx-cli
-              cargo-expand
-              dive
-            ]
-            ++ buildInputs
-            ++ naitiveBuildInputs;
-        };
+          buildInputs = with pkgs; [
+            just
+            dive
 
-        packages.default = naersk.buildPackage {
-          name = "newsletter";
-          version = "0.0.0";
-          src = ./.;
+            # Golang
+            go
+            go-tools
+            gotools
+            gopls
 
-          inherit buildInputs naitiveBuildInputs;
-        };
-
-        packages.public-dir = pkgs.runCommand "public-dir" {src = ./public;} "mkdir -p $out; cp -r $src/* $out";
-
-        packages.docker = pkgs.dockerTools.buildLayeredImage {
-          name = "newsletter-docker";
-          tag = "latest";
-
-          config = {
-            Env = [
-              "SSL_CERT_FILE=${pkgs.cacert}/etc/ssl/certs/ca-bundle.crt"
-              "NEWSLETTER_PUBLIC=${self'.packages.public-dir}"
-            ];
-            Cmd = ["${self'.packages.default}/bin/newsletter"];
-          };
+            postgresql
+          ];
         };
 
         formatter = inputs'.alejandra.packages.default;
