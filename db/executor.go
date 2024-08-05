95904f8c8ddda99c36717ecb2ade5b7d838a3b44
foehammer127 <foehammer@disroot.org>
2024-08-04 20:34:22 -0500

    Add Complete Database Interface
    Finish adding the complete database interface, including QueryInTx and
    ExecInTx functions.

--- db/executor.go
+++ db/executor.go
@@ -2,9 +2,13 @@ package db
 
 import (
 	"context"
+	"errors"
+	"fmt"
+	"log/slog"
 
 	"github.com/jackc/pgx/v5"
 	"github.com/jackc/pgx/v5/pgconn"
+	"github.com/jackc/pgx/v5/pgxpool"
 )
 
 // Generic database executor that can be passed around the application.
@@ -13,7 +17,7 @@ type Executor interface {
 	Query(context.Context, string, ...interface{}) (pgx.Rows, error)
 	QueryRow(context.Context, string, ...interface{}) pgx.Row
 
-	Begin(ctx context.Context) (Tx, error)
+	Begin(ctx context.Context) (pgx.Tx, error)
 }
 
 // Specific, transaction based executor that allows for committing and rolling back transactions.
@@ -22,4 +26,49 @@ type Tx interface {
 
 	Commit(ctx context.Context) error
 	Rollback(ctx context.Context) error
+}
+
+func Connect(ctx context.Context, url string) (*pgxpool.Pool, error) {
+	pool, err := pgxpool.New(ctx, url)
+	if err != nil {
+		return nil, err
+	}
+
+	return pool, nil
+}
+
+func ExecInTx(ctx context.Context, executor Executor, fun func(Tx) error) error {
+	newfun := func(t Tx) (struct{}, error) {
+		err := fun(t)
+		return struct{}{}, err
+	}
+
+	_, err := QueryInTx(ctx, executor, newfun)
+	return err
+}
+
+func QueryInTx[R any](ctx context.Context, executor Executor, fun func(Tx) (R, error)) (R, error) {
+	tx, err := executor.Begin(ctx)
+	if err != nil {
+		var result R
+		return result, fmt.Errorf("error beginning transaction: %w", err)
+	}
+	defer func() {
+		if e := tx.Rollback(ctx); e != nil && !errors.Is(e, pgx.ErrTxClosed) {
+			slog.Error("error rolling back transaction", "error", e)
+		}
+	}()
+
+	value, err := fun(tx)
+	if err != nil {
+		var result R
+		return result, err
+	}
+
+	if err := tx.Commit(ctx); err != nil {
+		var result R
+		return result, fmt.Errorf("error committing transaction: %w", err)
+	}
+
+	return value, nil
 }
