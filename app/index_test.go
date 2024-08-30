02b8c62060b5af7236de2ad415d28db89c764a85
foehammer127 <foehammer@disroot.org>
2024-08-29 22:23:00 -0500

    Add Index Test.
    Test if index page renders a 200OK.

---
+++ app/index_test.go
@@ -0,0 +1,31 @@
+package app_test
+
+import (
+	"context"
+	"net/http"
+	"net/http/httptest"
+	"testing"
+
+	"github.com/neilotoole/slogt"
+	"github.com/stretchr/testify/require"
+	"stpaulacademy.tech/newsletter/app"
+	"stpaulacademy.tech/newsletter/web"
+)
+
+func TestIndex(t *testing.T) {
+	t.Parallel()
+	ctx := context.Background()
+	p, err := web.NewPublic("../public")
+	require.NoError(t, err)
+
+	s := app.NewServer(slogt.New(t), p.Assets(), p.Templates())
+
+	rec := httptest.NewRecorder()
+
+	res, err := http.NewRequestWithContext(ctx, http.MethodGet, "/", nil)
+	require.NoError(t, err)
+
+	s.ServeHTTP(rec, res)
+
+	require.Equal(t, http.StatusOK, rec.Result().StatusCode)
+}
