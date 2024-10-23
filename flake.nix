c6f402335e756895f942b7e67a0ca9573643a45f
foehammer127 <foehammer@disroot.org>
2024-10-23 18:28:00 -0500

    Add source file filter to flake.nix.
    Add a source file filter to flake.nix, which prevents rebuilds to the
    main go package when other files change.

--- flake.nix
+++ flake.nix
@@ -31,11 +31,46 @@
           overlays = [];
         };
 
-        packages = {
+        packages = let
+          inherit (pkgs) lib;
+
+          isIncluded = includePaths: path: type: let
+            pathStr = toString path;
+            matchesPath = includePath: let
+              includePath' = toString includePath;
+            in
+              lib.hasPrefix includePath' pathStr || lib.hasPrefix pathStr includePath';
+          in
+            builtins.any matchesPath includePaths;
+
+          filterSourceFiles = root: includePaths: let
+            absolutePaths =
+              map (
+                path:
+                  if lib.hasPrefix "/" (toString path)
+                  then path
+                  else root + "/${toString path}"
+              )
+              includePaths;
+          in
+            builtins.filterSource (path: type: isIncluded absolutePaths path type) root;
+        in {
           default = pkgs.buildGoModule {
             name = "spa-newsletter";
 
-            src = ./.;
+            src = filterSourceFiles ./. [
+              ./app
+              ./cmd
+              ./config
+              ./cron
+              ./db
+              ./go.mod
+              ./go.sum
+              ./main.go
+              ./resource
+              ./util
+              ./web
+            ];
 
             vendorHash = "sha256-M53e6aFThhScGoDp4RqM5JQFhLHfOm5nV4RssfQ1Mdc=";
             doCheck = false;
